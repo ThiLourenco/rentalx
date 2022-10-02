71436110a27b16c65788f960b4f6f9022a9f018a
Thigo <thiago.l17@yahoo.com>
2022-10-02 18:05:25 -0300

    Adding: VSCode debugger

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,18 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3333;
 
 app.get("/", (req, res) => {
   return res.json({ message: "Hello, world!" });
+});
+
+app.post("/courses", (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
 });
 
 app.listen(PORT, () => {
