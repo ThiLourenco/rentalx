1d7e1fca405f154c502a0abe7919ffec2d9c771e
Thigo <thiago.l17@yahoo.com>
2022-10-02 21:42:05 -0300

    Adding model: Category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,24 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from "uuid";
+
+import { Category } from "../model/Category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidV4(),
+  const category = new Category();
+  Object.assign(category, {
     name,
     description,
-  };
+    created_at: new Date(),
+  });
 
   categories.push(category);
 
-  response.status(201).send();
+  return response.status(201).json({ category });
 });
 
 export { categoriesRoutes };
