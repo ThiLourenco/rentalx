37b8df71b7d674254c567447f5fae612feb58e10
Thigo <thiago.l17@yahoo.com>
2022-10-05 22:19:16 -0300

    Adding: Specification

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import express from "express";
 
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationsRoutes } from "./routes/Specification.routes";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/categories", categoriesRoutes);
-app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationsRoutes);
 
 const PORT = 3333;
 
