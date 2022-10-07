1a157bb11899308e3954a4e50a235d3e3fb6d2b8
Thigo <thiago.l17@yahoo.com>
2022-10-06 22:23:20 -0300

    Adding Singleton Pattern

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -3,9 +3,7 @@ import { CreateCategoryController } from "./CreateCategoryController";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 const categoriesRepository = CategoriesRepository.getInstance();
-
 const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
-
 const createCategoryController = new CreateCategoryController(
   createCategoryUseCase
 );
