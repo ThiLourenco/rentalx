b2ed4ad4839ca427a5b6769e03997451246c06d8
Thigo <thiago.l17@yahoo.com>
2022-10-23 21:02:26 -0300

    Adding: Migration Specification

--- src/database/migrations/1666569662891-CreateSpecifications.ts
+++ src/database/migrations/1666569662891-CreateSpecifications.ts
@@ -1,11 +1,7 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CreateSpecifications1666569662891 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
