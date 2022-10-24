fe96f8ebb2b7ad7df6e3d94431d8336fe8940e62
Thigo <thiago.l17@yahoo.com>
2022-10-23 21:14:01 -0300

    Adding: Migration Specification

--- src/database/migrations/1666569662891-CreateSpecifications.ts
+++ src/database/migrations/1666569662891-CreateSpecifications.ts
@@ -1,7 +1,39 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateSpecifications1666569662891 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "specifications",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "type",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("specifications");
+  }
 }
