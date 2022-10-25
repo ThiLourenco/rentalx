ad0e978be44526c082fcfd752c1cd0acd63222b8
Thigo <thiago.l17@yahoo.com>
2022-10-24 22:00:07 -0300

    Adding: Migration Users

---
+++ src/database/migrations/1666659290264-CreateUsers.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUsers1666659290264 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
