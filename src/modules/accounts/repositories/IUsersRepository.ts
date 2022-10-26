fc14ffdfbfeac239d0a4f3377b019a3fa7245bc2
Thigo <thiago.l17@yahoo.com>
2022-10-25 22:12:13 -0300

    Adding: bcrypt auth JWT

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -2,6 +2,7 @@ import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
 
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
+  findByName(email: string): Promise<void>;
 }
 
 export { IUsersRepository };
