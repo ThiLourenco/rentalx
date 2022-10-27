5012f7ce65aa6c7dc03619204aa88b5685eb1c83
Thigo <thiago.l17@yahoo.com>
2022-10-27 01:16:15 -0300

    Adding: authenticate in routes and findById

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
 export async function ensureAuthenticated(
   request: Request,
   response: Response,
@@ -15,8 +21,20 @@ export async function ensureAuthenticated(
   const [, token] = authHeader.split(" ");
 
   try {
-    const decoded = verify(token, "8c23d7b8d3c00ddb40961c856822aef8");
-    console.log(decoded);
+    const { sub: user_id } = verify(
+      token,
+      "8c23d7b8d3c00ddb40961c856822aef8"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exists!");
+    }
+
+    next();
   } catch {
     throw new Error("Invalid Token");
   }
--- src/routes/Specification.routes.ts
+++ src/routes/Specification.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
 const specificationsRoutes = Router();
 
 const createSpecification = new CreateSpecificationController();
-
+specificationsRoutes.use(ensureAuthenticated);
 specificationsRoutes.post("/", createSpecification.handle);
 
 export { specificationsRoutes };
