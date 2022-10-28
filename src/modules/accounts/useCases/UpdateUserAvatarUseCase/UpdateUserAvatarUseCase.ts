078652eea86c576a55a3d0b675142da6b82d9842
Thigo <thiago.l17@yahoo.com>
2022-10-27 21:45:31 -0300

    Adding: Column avatar in table users

---
+++ src/modules/accounts/useCases/UpdateUserAvatarUseCase/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,12 @@
+// Adicionar coluna avatar na tabela de users
+// Refatorar usuário com coluna avatar
+// Configuração upload multer
+// Criar regra de negócio do upload
+// Criar controller
+class UpdateUserAvatarUseCase {
+  async execute() {
+    //
+  }
+}
+
+export { UpdateUserAvatarUseCase };
