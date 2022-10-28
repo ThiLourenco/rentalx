bdd7bf6616e1aefae65771b0b7159670306fb428
Thigo <thiago.l17@yahoo.com>
2022-10-27 21:15:40 -0300

    Adding: handle error

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
+import "./database";
+
+import "./shared/container";
+
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
-
-import "./database";
-import "./shared/container";
 
 const app = express();
 
@@ -15,6 +18,20 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 const PORT = 3333;
 
