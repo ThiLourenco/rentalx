7fcadf281cda69dfe64170e2908876008c15dee8
Thigo <thiago.l17@yahoo.com>
2022-11-02 19:34:10 -0300

    Adding: Create Car

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,31 +1,45 @@
+import { Column, CreateDateColumn, JoinColumn, ManyToOne } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
+
+import { Category } from "./Category";
 
 class Car {
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   description: string;
 
+  @Column()
   daily_rate: string;
 
-  available: boolean;
+  @Column()
+  available: true;
 
+  @Column()
   license_plate: string;
 
+  @Column()
   fine_amount: number;
 
+  @Column()
   brand: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: "category_id" })
+  category: Category;
+
+  @Column()
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
     if (!this.id) {
       this.id = uuidV4();
-      this.available = true;
-      this.created_at = new Date();
     }
   }
 }
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,9 +2,11 @@ import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/accounts/infra/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
+import { ICarsRepository } from "@modules/cars/repositories/ICreateCarRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
@@ -21,3 +23,5 @@ container.registerSingleton<IUsersRepository>(
   "UserRepository",
   UsersRepository
 );
+
+container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
