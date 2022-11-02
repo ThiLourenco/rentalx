67f8fcd20299c11d04a89b879f0ba0b6b611a12e
Thigo <thiago.l17@yahoo.com>
2022-11-02 15:30:01 -0300

    Applying TDD: with CreateCarUseCase

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,25 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarRepositoryInMemory";
+
+import { CreateCarUseCase } from "./CreateCarUseCase";
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe("Create Car", () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
+  });
+
+  it("Should be able to create a new car", async () => {
+    await createCarUseCase.execute({
+      name: "Name Car",
+      description: "Description Car",
+      daily_rate: 100,
+      license_plate: "ABC_1234",
+      fine_amount: 60,
+      brand: "Brand",
+      category_id: "category",
+    });
+  });
+});
---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -0,0 +1,43 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICarsRepository } from "@modules/cars/repositories/ICreateCarRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+  daily_rate: string;
+  license_plate: string;
+  fine_amount: number;
+  brand: string;
+  category_id: string;
+}
+
+// @injectable()
+class CreateCarUseCase {
+  constructor(
+    // @inject("CarsRepository")
+    private CarsRepository: ICarsRepository
+  ) {}
+
+  async execute({
+    name,
+    description,
+    daily_rate,
+    license_plate,
+    fine_amount,
+    brand,
+    category_id,
+  }): Promise<void> {
+    this.CarsRepository.create({
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    });
+  }
+}
+
+export { CreateCarUseCase };
