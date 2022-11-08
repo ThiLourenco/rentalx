a80356dbaf20b71ee61588637e393ae9dccc0308
Thigo <thiago.l17@yahoo.com>
2022-11-07 21:02:42 -0300

    Create: migration CreateRentals

---
+++ src/shared/infra/typeorm/migrations/1667865725984-CreateRentals.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateRentals1667865725984 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
