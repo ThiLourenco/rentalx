77009b73d1f1bd418db19bad1d481e768e60e5d0
Thigo <thiago.l17@yahoo.com>
2022-11-07 21:27:24 -0300

    Created: migration CreateRentals

--- src/shared/infra/typeorm/migrations/1666659290264-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1666659290264-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1666659290264 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
