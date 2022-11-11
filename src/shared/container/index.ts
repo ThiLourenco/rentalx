d21934670c75c3cda3f45683315a7cbfe766f753
Thigo <thiago.l17@yahoo.com>
2022-11-10 23:22:57 -0300

    Adding: container rentals

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -10,6 +10,8 @@ import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRep
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { ICarsRepository } from "@modules/cars/repositories/ICreateCarRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
+import { RentalsRepository } from "@modules/rentals/infra/typeorm/repositories/RentalsRepository";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -29,6 +31,11 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<ICarsImagesRepository>(
   "CarsImagesRepository",
   CarsImagesRepository
+);
+
+container.registerSingleton<IRentalsRepository>(
+  "RentalsRepository",
+  RentalsRepository
 );
 
 container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
