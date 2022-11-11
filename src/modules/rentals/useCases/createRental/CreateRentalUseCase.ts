ba2947e0700e616120abe294e0da50a77d3f7ffd
Thigo <thiago.l17@yahoo.com>
2022-11-10 22:45:03 -0300

    Adding: RentalUseCase

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -42,8 +42,8 @@ class CreateRentalUseCase {
     const dateNow = this.dateProvider.dateNow();
 
     const compare = this.dateProvider.compareInHours(
-      expected_return_date,
-      dateNow
+      dateNow,
+      expected_return_date
     );
 
     if (compare < miniumHour) {
