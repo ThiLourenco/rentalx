635179246b207362e907e9b0a755d879e015d3ce
Thigo <thiago.l17@yahoo.com>
2022-11-10 23:40:03 -0300

    adding: entities and ajust migration

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,22 +1,39 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+@Entity("rentals")
 class Rental {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   car_id: string;
 
+  @Column()
   user_id: string;
 
+  @CreateDateColumn()
   start_date: Date;
 
+  @CreateDateColumn()
   end_date: Date;
 
+  @CreateDateColumn()
   expected_return_date: Date;
 
+  @Column()
   total: number;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 
   constructor() {
