365c1ab4254621ff86e4a6739a9d5f945cbee102
Thigo <thiago.l17@yahoo.com>
2022-11-16 20:41:58 -0300

    feat(): adding migration CreateUsersToken

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -26,20 +26,18 @@ class AuthenticateUserUseCase {
   ) {}
 
   async execute({ email, password }: IRequest): Promise<IResponse> {
-    // user exists
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
       throw new AppError("Email or password incorrect !", 401);
     }
-    // password is valid?
+
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
       throw new AppError("Email or password incorrect !", 401);
     }
 
-    // create jsonwebtoken
     const token = sign({}, "8c23d7b8d3c00ddb40961c856822aef8", {
       subject: user.id,
       expiresIn: "1d",
