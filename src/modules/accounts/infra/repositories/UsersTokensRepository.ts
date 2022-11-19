382e957d60a913ad3dc08d33dd2bd642a2b906b3
Thigo <thiago.l17@yahoo.com>
2022-11-19 08:10:58 -0300

    feat(): ResetPassword useCase

--- src/modules/accounts/infra/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/repositories/UsersTokensRepository.ts
@@ -42,6 +42,12 @@ class UsersTokensRepository implements IUsersTokensRepository {
   async deleteById(id: string): Promise<void> {
     await this.repository.delete(id);
   }
+
+  async findByRefreshToken(refresh_token: string): Promise<UserTokens> {
+    const userTokens = await this.repository.findOne({ refresh_token });
+
+    return userTokens;
+  }
 }
 
 export { UsersTokensRepository };
