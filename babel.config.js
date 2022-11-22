f046bf0601763c0169d7003002f50c2d28a3139e
Thigo <thiago.l17@yahoo.com>
2022-11-21 21:37:35 -0300

    feat(): adding babel.config

--- babel.config.js
+++ babel.config.js
@@ -4,18 +4,20 @@ module.exports = {
     "@babel/preset-typescript",
   ],
   plugins: [
-    "module-resolver",
-    {
-      alias: {
-        "@modules": ["./src/modules"],
-        "@config": ["./src/config"],
-        "@shared": ["./src/shared"],
-        "@errors": ["./src/errors"],
-        "@utils": ["./src/utils"]
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@modules": ["./src/modules"],
+          "@config": ["./src/config"],
+          "@shared": ["./src/shared"],
+          "@errors": ["./src/errors"],
+          "@utils": ["./src/utils"]
+        },
       },
-    },
-    "babel-plugin-transform-typescript-metadata",
-    ["@babel/plugin-proposal-decorators", { legacy: true }],
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
+      "babel-plugin-transform-typescript-metadata",
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ]
   ],
 }
