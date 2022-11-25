074b5094a8ddc6c68b328a0d74ce044740199f84
Thigo <thiago.l17@yahoo.com>
2022-11-25 00:56:15 -0300

    feat(): adding rate-limiter

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -9,6 +9,7 @@ import "@shared/container";
 
 import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppError";
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 import { router } from "@shared/infra/http/routes";
 import createConnection from "@shared/infra/typeorm";
 
@@ -16,6 +17,8 @@ import swaggerFile from "../../../swagger.json";
 
 createConnection();
 const app = express();
+
+app.use(rateLimiter);
 
 app.use(express.json());
 
