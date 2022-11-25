5e7bf321d762f77e36a83fb0b00f316d0ce29f7b
Thigo <thiago.l17@yahoo.com>
2022-11-25 01:22:14 -0300

    feat(): adding sentry for alerts

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,6 +5,9 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
 import "@shared/container";
 
 import upload from "@config/upload";
@@ -18,9 +21,21 @@ import swaggerFile from "../../../swagger.json";
 createConnection();
 const app = express();
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 app.use(rateLimiter);
 
 app.use(express.json());
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
@@ -29,6 +44,17 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(router);
+
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      if (error.status === 429 || error.status === 500) {
+        return true;
+      }
+      return false;
+    },
+  })
+);
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
